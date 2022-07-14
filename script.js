3b386b5301f543591a76c255ffc809d876f071c8
manu <manucbhat608@gmail.com>
2022-07-14 16:56:37 +0530

    started js

---
+++ script.js
@@ -0,0 +1,7 @@
+//Generate a random number between 1 and 100.
+let randomNumber = parseInt((Math.random()*100) + 1);
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const startOver = document.querySelector('.resultParas');
